7be52a9902e3519af175b79f8081529b0043968f
Ahexo <ahexo@ciencias.unam.mx>
2025-12-02 10:58:14 -0600

    [P1] First comments on my reasoning

---
+++ 01.py
@@ -0,0 +1,15 @@
+# Problem 1
+# https://adventofcode.com/2025/day/1
+
+"""
+INPUT:
+
+A list of strings of the regular form: `(L|R)([0-9+])`
+
+1. These are rotations in a modulo 99, L to the left and R to the right.
+2. Number of steps.
+
+"""
+
+
+if __name__ == "__main__":
